b68b09325bcaaa4f829d25c25b69e83db14f8101
Red-Kyurem <50855216+Red-Kyurem@users.noreply.github.com>
2024-03-05 23:36:24 -0500

    pressing button 1 (v) now rotates the stick towards fire

--- Assets/Team18/Scripts/InputManager.cs
+++ Assets/Team18/Scripts/InputManager.cs
@@ -1,34 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace team18
 {
     public class InputManager : MicrogameInputEvents
     {
         GameObject campfire;
+
+        public Vector2 direction;
+        bool button1Held = false;
+        bool button2Held = false;
+
+        HingeJoint hingeJoint;
+
         // Start is called before the first frame update
         void Start()
         {
             // Tag0 is the campfire
             campfire = GameObject.FindGameObjectWithTag("Tag0");
 
+            hingeJoint = GetComponent<HingeJoint>();
+            hingeJoint.useMotor = false;
+
             // the position the marshmellow stick will look at
             Vector3 lookAtPos = campfire.transform.position;
             lookAtPos.y = transform.position.y;
 
             transform.LookAt(lookAtPos);
+            Debug.Log("hello!");
         }
 
         // Update is called once per frame
         void Update()
         {
+            direction = GetStickDirection();
 
+            button1Held = GetButton1Input();
+            button2Held = GetButton2Input();
+
+            if (button1Held)
+            {
+                hingeJoint.useMotor = true;
+            }
+            else
+            { 
+                hingeJoint.useMotor = false;
+            }
         }
 
-        void DetectSpinning()
+        Vector2 GetStickDirection()
         { 
-        
+            return stick.normalized;
+        }
+
+        bool GetButton1Input()
+        {
+            if (button1.IsPressed())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool GetButton2Input()
+        {
+            if (button2.IsPressed())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
