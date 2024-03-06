f6bfb7be9758f3a8be8ebdde84ae0065264b46db
Red-Kyurem <50855216+Red-Kyurem@users.noreply.github.com>
2024-03-06 01:26:42 -0500

    Fire can now damage marshmallow

---
+++ Assets/Team18/Scripts/BurnableAsset.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace team18
+{
+    public class BurnableAsset : MonoBehaviour
+    {
+
+        public float currentHealth = 0;
+        public float maxHealth = 100;
+
+        public Gradient gradient;
+        Material material;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentHealth = maxHealth;
+            material = GetComponent<MeshRenderer>().material;
+
+
+            ChangeGradientColour();
+        }
+
+
+        public void RemoveHealth(float amount)
+        {
+            currentHealth -= amount * Time.deltaTime;
+
+            ChangeGradientColour();
+        }
+
+        // changes colour based on the object's remaining health
+        void ChangeGradientColour()
+        {
+            float healthProgress = currentHealth / maxHealth;
+
+            material.color = gradient.Evaluate(1 - healthProgress);
+            
+        }
+    }
+}
---
+++ Assets/Team18/Scripts/FireDamage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace team18
+{
+    public class FireDamage : MonoBehaviour
+    {
+        public float damageAmount = 0;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            RaycastHit[] hits = Physics.BoxCastAll(transform.position, transform.lossyScale/2, Vector3.down, Quaternion.identity, 1, LayerMask.GetMask("Detect Hazard"));
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.GetComponent<BurnableAsset>())
+                {
+                    BurnableAsset BA = hit.collider.GetComponent<BurnableAsset>();
+                    BA.RemoveHealth(damageAmount);
+                }
+            }
+
+        }
+    }
+}
