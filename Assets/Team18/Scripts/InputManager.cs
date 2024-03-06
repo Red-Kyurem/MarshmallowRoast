161da8cfa83439cadc5cf4b3b83ea9224c76a87e
Red-Kyurem <50855216+Red-Kyurem@users.noreply.github.com>
2024-03-05 22:00:50 -0500

    marshmallow stick will face the fire when the scene starts

--- Assets/Team18/Scripts/InputManager.cs
+++ Assets/Team18/Scripts/InputManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MicrogameInputEvents
+namespace team18
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InputManager : MicrogameInputEvents
     {
-        
-    }
+        GameObject campfire;
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Tag0 is the campfire
+            campfire = GameObject.FindGameObjectWithTag("Tag0");
 
-    // Update is called once per frame
-    void Update()
-    {
+            // the position the marshmellow stick will look at
+            Vector3 lookAtPos = campfire.transform.position;
+            lookAtPos.y = transform.position.y;
+
+            transform.LookAt(lookAtPos);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void DetectSpinning()
+        { 
         
+        }
     }
 }
